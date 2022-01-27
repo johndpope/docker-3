cd033ca3caa45cf3548e8aecadb1425f3d7840f3
Alexander Broll <alexbroll1302@gmail.com>
2022-01-27 15:50:17 +0100

    comments and changes in find_and_replace.py before find_and_replace

--- 01_scripts/modules/os_tools/find_and_replace.py
+++ 01_scripts/modules/os_tools/find_and_replace.py
@@ -2,6 +2,7 @@ import os
 import glob
 from distutils.util import strtobool
 
+
 def _ask_yes_no(question: str) -> bool:
     """Ask the user the question until the user inputs a valid answer."""
     while True:
@@ -11,13 +12,21 @@ def _ask_yes_no(question: str) -> bool:
         except ValueError:
             pass
 
-def find_and_replace(old_string: str, new_string: str, search_dir: str, search_files: str, recursive: bool =  True):
+
+def find_and_replace(old_string: str,
+                     new_string: str,
+                     search_dir: str,
+                     search_condition: str,
+                     replace_count: int = -1,
+                     recursive: bool = True):
     """
-    Recursively (default) finds and replaces all occurences of old_string with new_string in all specified files in the search_dir
+    Recursively (default) finds and replaces all occurences (default) of old_string with new_string in all files that match the search_condition in the search_dir
 
     if recursive == False: os.path.join(search_dir, "folder", "file.py") won't be found.
 
-    Examples for search_files:
+    if replace_count == 1: Only the first (etc.) occurence of old_string in every file will be replaced, -1 (default) replaces all occurences
+
+    Examples for search_condition:
 
     All python files: "*.py"
 
@@ -25,36 +34,52 @@ def find_and_replace(old_string: str, new_string: str, search_dir: str, search_f
 
 
     """
-    files = glob.glob(os.path.join(search_dir, "**", search_files), recursive=recursive)
+    # Get a list of all files which match the search_condition criteria
+    files = glob.glob(os.path.join(search_dir, "**", search_condition),
+                      recursive=recursive)
+
+    # Init empty list for relevant files
     replace_file_list = []
-    print(__file__)
+
     for file in files:
+        # Exclude this file and all folders. Only open files
         if not os.path.samefile(file, __file__) and os.path.isfile(file):
             with open(file) as r:
+                # Get all text
                 text = r.read()
                 if old_string in text:
+                    # Append the file to list if it contains old_string
                     replace_file_list.append(file)
 
-    print(f"{old_string} was found in the following {len(replace_file_list)} files: \n")
+    print(
+        f"{old_string} was found in the following {len(replace_file_list)} files: \n"
+    )
     [print(f"{ctr}: {file}") for ctr, file in enumerate(replace_file_list)]
 
     if _ask_yes_no(f"\nContinue?\n"):
+        # Possibility to skip indices as long as replace_file_list is not empty
         while replace_file_list:
             if _ask_yes_no("\nExclude files? See indices above."):
                 replace_file_list.pop(int(input("Skip Index: \n")))
-                [print(f"{ctr}: {file}") for ctr, file in enumerate(replace_file_list)]
+                [
+                    print(f"{ctr}: {file}")
+                    for ctr, file in enumerate(replace_file_list)
+                ]
             else:
-                break  
+                break
 
+        # Open all the remaining files in replace_file_list
         for file in replace_file_list:
             with open(file) as r:
                 text = r.read()
-            text_new = text.replace(old_string, new_string)
+            # Replace all replace_count occurences of old_string with new_string
+            text_new = text.replace(old_string,
+                                    new_string,
+                                    __count=replace_count)
             with open(file, "w") as w:
                 w.write(text_new)
 
         print(f"{len(replace_file_list)} files edited.")
-
     else:
         print("Aborted.")
 
@@ -63,5 +88,10 @@ if __name__ == "__main__":
     old_string = 'sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "modules"))'
     new_string = 'sys.path.append(os.path.join(os.path.dirname(__file__).split("01_scripts")[0], "01_scripts", "modules"))'
     search_dir = r"G:\docker\01_scripts"
-    search_files = "*.py"
-    find_and_replace(old_string, new_string, search_dir, search_files, recursive=True)
+    search_condition = "*.py"
+    find_and_replace(old_string=old_string,
+                     new_string=new_string,
+                     search_dir=search_dir,
+                     search_condition=search_condition,
+                     replace_count=-1,
+                     recursive=True)
